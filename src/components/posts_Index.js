554da4da129ba9db494eedbf13ffabb2e815ccc7
manel24 <manelcharouni@think-it.io>
2018-07-05 15:49:12 +0100

    setting up the default route for blog posts

---
+++ src/components/posts_Index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class PostsIndex extends Component {
+  render() {
+    return <div>POsts index</div>;
+  }
+}
+export default PostsIndex;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+import PostsIndex from './components/posts_Index'
+
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <BrowserRouter>
+      <div>
+       <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector(".container")
+);
