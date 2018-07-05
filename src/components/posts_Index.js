5929879f761e202a2d97f07470edd84d49e8ab2e
manel24 <manelcharouni@think-it.io>
2018-07-05 18:05:28 +0100

    issuing the action creator on component mount

--- src/components/posts_Index.js
+++ src/components/posts_Index.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/index";
 
 class PostsIndex extends Component {
+
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
   render() {
     return <div>POsts index</div>;
   }
 }
-export default PostsIndex;
+export default connect(
+  null,
+  { fetchPosts }
+)(PostsIndex);
