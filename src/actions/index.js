8d00de3d0db5cd2352c3cde445bcc1e6c418e368
manel24 <manelcharouni@think-it.io>
2018-07-05 17:28:31 +0100

    implementing the fetchPosts action creator

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const FETCH_POSTS = "fetch_posts";
+
+const BASE_URL='http://reduxblog.herokuapp.com/api';
+const API_KEY='?key=Manel12345'
+
+export function fetchPosts() {
+    const request = axios.get(`${BASE_URL}/posts${API_KEY}`);
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+}
--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Route } from "react-router-dom";
+import promise from "redux-promise";
 
-import PostsIndex from './components/posts_Index'
+import PostsIndex from "./components/posts_Index";
 
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-       <Route path="/" component={PostsIndex} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>,
