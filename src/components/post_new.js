1ec8e17ce4569c84632de04d0fd14e544d7e98ab
manel24 <manelcharouni@think-it.io>
2018-07-08 22:06:08 +0100

    Handling form submittal

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -5,18 +5,53 @@ class PostNew extends Component {
   renderField(field) {
     return (
       <div className="form-group">
-      <label>{field.label}</label>
-        <input  className="form-control" type="text" {...field.input} />
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        {field.meta.error}
       </div>
     );
   }
+  //{field.meta.error}//this will be the error msg returned (if any) from the validate fx (the name property wires things up)
+  onSubmit(values) {
+    //this === component
+    console.log(values);
+  }
   render() {
-    return <form>
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field label="Title" name="title" component={this.renderField} />
-        <Field label="tags" name="tags" component={this.renderField} />
-        <Field label="Post content" name="content" component={this.renderField} />
-      </form>;
+        <Field
+          label="Catgories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    );
   }
 }
-
-export default reduxForm({ form: "NewPostForm" })(PostNew);
+function validate(values) {
+  //cnsole.log(values) => {title:'cnjf', categories:'cdnjcnjc',content:'njnj'}
+  const error = {};
+  if (!values.title) {
+    error.title = "enter a title!";
+  }
+  if (!values.categories) {
+    error.categories = "enter a category!";
+  }
+  if (!values.content) {
+    error.content = "enter a content!";
+  }
+  return error;
+  // if errors is empty, the form is fine to sumbit
+  // if errors haas *any* of the properties not empty, redux form assumes the fors is not valid
+}
+export default reduxForm({ validate: validate, form: "NewPostForm" })(PostNew);
