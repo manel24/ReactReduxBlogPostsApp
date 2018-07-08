5165bef71ae16c97a9fe261fa39bb21b11251522
manel24 <manelcharouni@think-it.io>
2018-07-08 16:30:18 +0100

    fixing the React Router issue

---
+++ src/components/post_new.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PostNew extends Component {
+  render() {
+    return <div>New post! </div>;
+  }
+}
+
+export default PostNew;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import promise from "redux-promise";
 
 import PostsIndex from "./components/posts_Index";
+import PostNew from "./components/post_new";
 
 import reducers from "./reducers";
 
@@ -15,7 +16,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
